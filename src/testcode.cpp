1837255109c214736259fcc648c113c39b6dd0ae
Jonas Minnberg <sasq64@gmail.com>
2019-04-24 17:42:39 +0200

    Added some more testcode

--- src/testcode.cpp
+++ src/testcode.cpp
@@ -1,8 +1,32 @@
 #include <fmt/format.h>
+#include <vector>
+
+struct Vector
+{
+    float x;
+    float y;
+    float z;
+};
+
+class Camera
+{
+    int fov;
+
+public:
+    Vector pos;
+
+    Camera(int fov) : fov(fov) {}
+};
 
 int doSomething(int code)
 {
-    return code * -2;
+    Camera cam{3};
+    int total = 0;
+    std::vector<int> s{1, 2, 3, 4};
+    for (unsigned i = 0; i < s.size(); i++)
+        total += s[i];
+
+    return total * code * -2;
 }
 
 int main()
